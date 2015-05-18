fe15ade4620092e90a2a3d36b24d776a314499cf
Spardason <eric-vaillancourt@hotmail.ca>
2015-05-17 20:02:03 -0400

    Added consts for all the rooms offset and commentaries for the said function

--- Object.cpp
+++ Object.cpp
@@ -1,5 +1,8 @@
 #include "Object.h"
 
+
+
+
 Object::Object()
 	: Animation()
 {
--- TileManager.h
+++ TileManager.h
@@ -41,5 +41,26 @@ protected:
 	std::vector<Sprite*>::iterator iterWalls;
 	Tile *tiles[TOTAL_TILES];
 	bool isInitialized;
+
+	// Consts for the rooms offset, Yeah! I know it's bad, but in this case I had to make it work fast
+	// and without magic numbers so here's what I did !
+	const point<int> OFFSET_ROOM_1 =			{732, 864};
+	const point<int> OFFSET_ROOM_2 =			{492, 864};
+	const point<int> OFFSET_ROOM_3 =			{972, 864};
+	const point<int> OFFSET_ROOM_4 =			{732, 864};
+	const point<int> OFFSET_ROOM_5 =			{492, 544};
+	const point<int> OFFSET_ROOM_6 =			{732, 544};
+	const point<int> OFFSET_ROOM_7 =			{972, 544};
+	const point<int> OFFSET_ROOM_8 =			{492, 384};
+	const point<int> OFFSET_ROOM_9 =			{732, 384};
+	const point<int> OFFSET_ROOM_10 =			{972, 384};
+	const point<int> OFFSET_ROOM_11 =			{1212, 384};
+	const point<int> OFFSET_ROOM_12 =			{732, 224};
+	const point<int> OFFSET_ROOM_13 =			{732, 64};
+	const point<int> OFFSET_ROOM_14 =			{492, 64};
+	const point<int> OFFSET_ROOM_BOSS =			{1212, 224};
+	const point<int> OFFSET_ROOM_SHOP =			{252, 384};
+	const point<int> OFFSET_ROOM_TRIFORCE =		{1452, 224};
+	const point<int> OFFSET_ROOM_UNDERGROUND =	{252, 64};
 };
 
